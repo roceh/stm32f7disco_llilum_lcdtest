60b30125d5e6d1fcc0c1bb8abe8a7ac29235636d
David <davdun@gmail_com>
2015-10-17 09:17:57 +0100

    Basic demo of driving the LCD via C#

---
+++ LCDTest/Native/helpers.h
@@ -0,0 +1,27 @@
+//
+// Copyright (c) Microsoft Corporation.    All rights reserved.
+//
+
+//
+// Platform dependent configurations
+//
+#ifdef TARGET_LPC1768
+#include "LPC17xx.h"
+#elif TARGET_K64F
+#include "MK64F12.h"
+#elif TARGET_DISCO_F746NG
+#include "stm32f746xx.h"
+#else
+#error Undefined plaform
+#endif
+
+#include "gpio_api.h"
+#include "spi_api.h"
+#include "i2c_api.h"
+#include "core_cmFunc.h"
+#include "us_ticker_api.h"
+#include "serial_api.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+
